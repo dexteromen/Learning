7ac3f41150e550ee3cda874821d12ceb1612dbf3
dexteromen <himanshu@xenonstack.com>
2025-02-24 19:54:15 +0530

    Fix DueDate format parsing in CreateTask handler

--- go_task/tasks/handlers/task_handlers.go
+++ go_task/tasks/handlers/task_handlers.go
@@ -21,10 +21,10 @@ func CreateTask(c *gin.Context) {
 	}
 
 	if task.DueDate != "" {
-		dueDate, err := time.Parse("2006-01-02", task.DueDate)
+		dueDate, err := time.Parse("02-01-2006", task.DueDate)
 		if err != nil {
 			log.Println("Error parsing DueDate:", err)
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid DueDate format: YYYY-MM-DD"})
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid DueDate format: DD-MM-YYYY"})
 			return
 		}
 		task.DueDate = dueDate.Format("02-01-2006")
